a3b661b7c2aef94b1a3d4c5b6c89fb19b3545981
Humberto Sales <betofoxmal@gmail.com>
2023-10-30 01:56:44 -0300

    update ano com js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 const menuListItens = document.querySelectorAll('.li-menu');
 
+document.getElementById('anoatual').innerHTML = new Date().getFullYear();
+
 function selectMenu() {
     menuListItens.forEach((liitens) => 
         liitens.classList.remove('activa')
