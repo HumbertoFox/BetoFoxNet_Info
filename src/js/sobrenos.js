8609134d250218f4fc89862547d276921b345428
Humberto Sales <betofoxmal@gmail.com>
2024-01-06 04:14:06 -0300

    update JS

--- src/js/sobrenos.js
+++ src/js/sobrenos.js
@@ -1,8 +1,20 @@
 'user strict'
 
+const selectMain = document.querySelector('.main-sobrenos');
 const selectDivs = document.querySelectorAll('.div-main');
-
+const selectBtnRight = document.querySelector('.btn-right');
+const selectBtnLeft = document.querySelector('.btn-left');
 let slideAtual = 0;
+
+selectMain.addEventListener("mouseenter", () => {
+    selectBtnRight.style.opacity = ".3";
+    selectBtnLeft.style.opacity = ".3";
+});
+
+selectMain.addEventListener("mouseout", () => {
+    selectBtnRight.style.opacity = "0";
+    selectBtnLeft.style.opacity = "0";
+});
 
 const constInterval = setInterval(slideSobreNos, 5000);
 
