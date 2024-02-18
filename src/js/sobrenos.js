3c00da4d8dd69e96a241eacc7cc90ceb7491cd80
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-02-18 17:42:01 -0300

    update h1, h2, p sobre nós

--- src/js/sobrenos.js
+++ src/js/sobrenos.js
@@ -4,18 +4,39 @@ const selectMain = document.querySelector('.main-sobrenos');
 const selectDivs = document.querySelectorAll('.div-main');
 const selectBtnRight = document.querySelector('.btn-right');
 const selectBtnLeft = document.querySelector('.btn-left');
+const selectTagH1 = document.querySelector('.h1-info-nos');
+const selectTagP = document.querySelector('.p-info-nos');
+const selectTagH2 = document.querySelectorAll('.h2-info-nos');
 const maxSlides = selectDivs.length;
 let slideAtual = 0;
 
 selectMain.addEventListener("mouseenter", () => {
-    selectBtnRight.style.opacity = ".3";
-    selectBtnLeft.style.opacity = ".3";
+    opacityButtonEnter();
 });
 
 selectMain.addEventListener("mouseout", () => {
     selectBtnRight.style.opacity = "0";
     selectBtnLeft.style.opacity = "0";
 });
+
+selectTagP.addEventListener("mouseenter", () => {
+    opacityButtonEnter();
+});
+
+selectTagH1.addEventListener("mouseenter", () => {
+    opacityButtonEnter();
+});
+
+selectTagH2.forEach((selectH2) => {
+    selectH2.addEventListener("mouseenter", () => {
+        opacityButtonEnter();
+    })
+});
+
+function opacityButtonEnter() {
+    selectBtnRight.style.opacity = ".3";
+    selectBtnLeft.style.opacity = ".3";
+};
 
 setInterval(slideSobreNos, 5000);
 
@@ -25,7 +46,7 @@ function slideSobreNos() {
 
     slideAtual++;
 
-    if (slideAtual > maxSlides -1) {
+    if (slideAtual > maxSlides - 1) {
         slideAtual = 0;
     };
 
@@ -36,7 +57,7 @@ selectBtnLeft.addEventListener("click", () => {
     selectDivs[slideAtual].classList.remove('visibled');
     slideAtual -= 1;
     if (slideAtual < 0) {
-        slideAtual = maxSlides -1;
+        slideAtual = maxSlides - 1;
     };
     selectDivs[slideAtual].classList.add('visibled');
 });
