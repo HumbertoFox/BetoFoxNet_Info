9c0056bda4c918f1774ea1508cf225978746a753
Humberto Ribeiro <humbertoemaus@gmail.com>
2025-04-16 23:10:38 -0300

    feat

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -5,18 +5,16 @@ import Gallery0 from '@/assets/App_Laravel.png';
 import Gallery1 from '@/assets/App_Next_Js.png';
 import Gallery2 from '@/assets/App_Next_Js01.png';
 import Gallery3 from '@/assets/App_Next_Js02.png';
+import Gallery4 from '@/assets/Consult0.png';
+import Gallery5 from '@/assets/donor_form.png';
+import Gallery6 from '@/assets/list_donations.png';
 import Image from 'next/image';
-import {
-    Swiper,
-    SwiperSlide
-} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import {
-    useEffect,
-    useRef
-} from 'react';
+import { useEffect, useRef } from 'react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import gsap from 'gsap';
+import { Autoplay, Keyboard } from 'swiper/modules';
 
 export default function GalleryComponent() {
     const gallerySectionRef = useRef(null);
@@ -56,11 +54,7 @@ export default function GalleryComponent() {
         });
     }, []);
     return (
-        <section
-            className={styles.gallery}
-            id='galeria'
-            ref={gallerySectionRef}
-        >
+        <section className={styles.gallery} id='galeria' ref={gallerySectionRef}>
             <h3 ref={galleryTitleRef}>Galeria</h3>
             <Swiper
                 className={styles.mySwiper}
@@ -68,31 +62,31 @@ export default function GalleryComponent() {
                 spaceBetween={50}
                 loop={true}
                 grabCursor={true}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
+                keyboard={{ enabled: true }}
+                modules={[Keyboard, Autoplay]}
                 ref={gallerySwiperRef}
             >
                 <SwiperSlide>
-                    <Image
-                        src={Gallery0}
-                        alt='Imagem Projeto Laravel'
-                    />
+                    <Image src={Gallery0} alt='Imagem Projeto Laravel' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        src={Gallery1}
-                        alt='Imagem Projeto Next Js 1'
-                    />
+                    <Image src={Gallery5} alt='Imagem Projeto Laravel' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        src={Gallery2}
-                        alt='Imagem Projeto Next Js 2'
-                    />
+                    <Image src={Gallery6} alt='Imagem Projeto Laravel' />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Image
-                        src={Gallery3}
-                        alt='Imagem Projeto Next Js 3'
-                    />
+                    <Image src={Gallery1} alt='Imagem Projeto Next Js 1' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image src={Gallery2} alt='Imagem Projeto Next Js 2' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image src={Gallery3} alt='Imagem Projeto Next Js 3' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image src={Gallery4} alt='Imagem Projeto Laravel' />
                 </SwiperSlide>
             </Swiper>
         </section>
