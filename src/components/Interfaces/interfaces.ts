2415e6001640fb6a26150eaa7b0385107f13d81b
Humberto Ribeiro <betofoxmal@gmail.com>
2025-09-29 02:57:46 -0300

    fix: icon props

--- src/components/Interfaces/interfaces.ts
+++ src/components/Interfaces/interfaces.ts
@@ -1,7 +1,6 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-
 export interface iconsProps {
-    icon: IconProp;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    icon: any;
     className?: string;
     [key: string]: unknown;
 };
